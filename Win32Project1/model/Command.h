4fbb168b12c3c61ace42fdf012e8bd825b5f4fe1
gsaha36 <gsaha36@gmail.com>
2015-08-20 18:00:14 +0530

    changes in command class

--- Win32Project1/model/Command.h
+++ Win32Project1/model/Command.h
@@ -5,6 +5,9 @@ namespace shape_maker {
 	class Shape;
 	class Command
 	{
+	private:
+		std::shared_ptr<Shape> currShape;
+		std::shared_ptr<Shape> oldShape;
 	public:
 		virtual void execute(std::shared_ptr<Shape> shape) = 0;
 		virtual void undo() = 0;
