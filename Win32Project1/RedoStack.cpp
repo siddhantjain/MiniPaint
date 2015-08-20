ae07a44b77e60cfe748990a8186c51ed25cd04ee
gsaha36 <gsaha36@gmail.com>
2015-08-20 18:39:09 +0530

    redo stack.cpp included

---
+++ Win32Project1/RedoStack.cpp
@@ -0,0 +1,17 @@
+#include "redoStack.h"
+#include "UndoStack.h"
+void RedoStack::push_command(shape_maker::CommandPtr commandObject)
+{
+	redostack.push(commandObject);
+}
+shape_maker::CommandPtr RedoStack::pop_command()
+{
+	if (!redostack.empty())
+	{
+		shape_maker::CommandPtr commandObject = redostack.top();
+		redostack.pop();
+		return commandObject;
+	}
+	else
+		return NULL;
+}
