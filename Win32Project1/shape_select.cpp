d0f3a972d9dad21baafe7943257b26a9d43ad45c
siddhant <post.siddhant@gmail.com>
2015-08-20 15:06:21 +0530

    change bounding box boundary thickness

---
+++ Win32Project1/shape_select.cpp
@@ -0,0 +1,21 @@
+#include "shape_select.h"
+
+int shape_select::which_shape(int x, int y)
+{
+	file_maker::File temp_file;
+	vector<shared_ptr<Shape>> shape_list = temp_file.get_shape_list();
+
+	for (int runner = 0; runner < shape_list.size(); runner++)
+	{
+		int temp_shape_tl_x = shape_list[runner]->get_topleft_x();
+		int temp_shape_tl_y = shape_list[runner]->get_topleft_y();
+		int temp_shape_br_x = shape_list[runner]->get_bottomright_x();
+		int temp_shape_br_y = shape_list[runner]->get_bottomright_y();
+		if ((x >= temp_shape_tl_x) && (x <= temp_shape_br_x) && (y >= temp_shape_tl_y) && (y <= temp_shape_br_y))
+		{
+			//hit shape object found //choose the latest drawn shape
+			shape_select::selected_shape_id = shape_list[runner]->get_shape_id();
+		}
+	}
+	return selected_shape_id;
+}
